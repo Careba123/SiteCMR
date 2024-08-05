8767b7076e90dd34f01d8e1ce8834fc4ae5c8bbf
Careba123 <criscareba@gmail.com>
2024-08-05 16:13:37 +0000

    Mon, Aug 5, 2024, 4:13 PM +00:00

--- cmr_project/cmr_app/urls.py
+++ cmr_project/cmr_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'cmr_app'
+
 urlpatterns = [
     path('upload/', views.upload_cmr, name='upload_cmr'),
     path('dashboard/', views.dashboard, name='dashboard'),
