4344d818e90e8cf26e0d75614e46b611c85c7cff
Careba123 <criscareba@gmail.com>
2024-08-06 15:20:23 +0000

    Tue, Aug 6, 2024, 6:20 PM +03:00

--- cmr_project/cmr_app/views.py
+++ cmr_project/cmr_app/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 from .models import CMR
 from .forms import CMRForm
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.models import User
-from .models import CMR
+
+@user_passes_test(lambda u: u.is_staff)
+def dashboard(request):
+    users = User.objects.all()
+    return render(request, 'cmr_app/dashboard.html', {'users': users})
+
+@user_passes_test(lambda u: u.is_staff)
+def user_documents(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    documents = CMR.objects.filter(user=user)
+    return render(request, 'cmr_app/user_documents.html', {'user': user, 'documents': documents})
 
 @login_required
 def upload_cmr(request):
@@ -20,16 +27,3 @@ def upload_cmr(request):
     else:
         form = CMRForm()
     return render(request, 'cmr_app/upload_cmr.html', {'form': form})
-
-@user_passes_test(lambda u: u.is_staff)
-def dashboard(request):
-    users = User.objects.all()
-    return render(request, 'myapp/dashboard.html', {'users': users})
-
-
-
-@user_passes_test(lambda u: u.is_staff)
-def user_documents(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    documents = CMR.objects.filter(user=user)
-    return render(request, 'myapp/user_documents.html', {'user': user, 'documents': documents})
