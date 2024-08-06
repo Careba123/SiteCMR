f9b21a75c5b783c549df2fda991c41b8e93c21d3
Careba123 <criscareba@gmail.com>
2024-08-06 23:08:42 +0000

    Wed, Aug 7, 2024, 2:08 AM +03:00

--- cmr_project/cmr_project/settings.py
+++ cmr_project/cmr_project/settings.py
@@ -112,3 +112,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Add this line at the end of the file
+LOGIN_REDIRECT_URL = '/dashboard/'
