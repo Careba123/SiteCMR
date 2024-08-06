e9823d5e1df665ba310dcac6380633c6dcba1aa0
Careba123 <criscareba@gmail.com>
2024-08-06 23:05:25 +0000

    Wed, Aug 7, 2024, 2:05 AM +03:00

--- cmr_project/cmr_project/settings.py
+++ cmr_project/cmr_project/settings.py
@@ -76,19 +76,15 @@ WSGI_APPLICATION = 'cmr_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'newdatabase',  # Numele noii baze de date
-        'USER': 'cristi',  # Numele noului utilizator
-        'PASSWORD': 'Cristi12345@',  # Parola noului utilizator
-        'HOST': 'localhost',  # De obicei localhost pentru serverul local de PostgreSQL
-        'PORT': '5432',  # Portul standard pentru PostgreSQL
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'cmr_app:dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+
 
 
 
