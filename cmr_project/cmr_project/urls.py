88a64b743675f7850c3eebb55c7d5df72f1d00e2
Careba123 <criscareba@gmail.com>
2024-08-06 23:21:30 +0000

    Wed, Aug 7, 2024, 2:21 AM +03:00

--- cmr_project/cmr_project/urls.py
+++ cmr_project/cmr_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.http import HttpResponseRedirect
+from django.conf import settings  # Importă setările
+from django.conf.urls.static import static  # Importă static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +12,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('accounts/profile/', lambda request: HttpResponseRedirect('/dashboard/')),
 ]
+
+# Adaugă configurarea pentru fișierele media
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
